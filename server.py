128e63c9ba14c1b75e3a76baa903335868eb0f39
Lukas Weihrauch <weihrauch.business@gmail.com>
2022-11-15 11:46:32 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, abort
+from subprocess import call
+
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print("THE WEBOOK WORKED")
+        call('cd nextjs_ssg; yarn build', shell=True)
+        return 'success', 200
+    else:
+        abort(400)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8000')
