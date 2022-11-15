e656c97eba9a0c87ae04a81937f68e5f9939b2c9
Lukas Weihrauch <weihrauch.business@gmail.com>
2022-11-15 18:30:28 +0100

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def webhook():
     if request.method == 'POST':
         print("THE WEBOOK WORKED")
-        call('cd nextjs_ssg; kill %1; npm run build; npm run start &', shell=True)
+        call('kill $(lsof -t -i:3000);cd nextjs_ssg; npm run build; npm run start', shell=True)
         return 'success', 200
     else:
         abort(400)
