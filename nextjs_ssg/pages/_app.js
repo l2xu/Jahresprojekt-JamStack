6934e48228f079e00625618453c8d097e42af83b
JessicaMartina <u36722@hs-harz.de>
2022-12-06 18:49:37 +0100

    Button_ScrollToTop

--- nextjs_ssg/pages/_app.js
+++ nextjs_ssg/pages/_app.js
@@ -3,11 +3,27 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
+
+     /* if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btn_totop.style.display = "block";
+    } else {
+      btn_totop.style.display = "none";
+    }
+  */
+
+  // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
   return (
     <div className="wrapper">
       <Header />
       <Component {...pageProps} />
       <Footer />
+      <img id="btn_totop" src="/next (1).png" onClick={topFunction} className="btn_scroll" width="40px"/>
     </div>
   );
 }
