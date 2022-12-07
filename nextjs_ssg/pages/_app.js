9bf7128f060efde5a7f47d6c1b577f5849100e3f
l2xu <weihrauch.business@gmail.com>
2022-12-07 17:06:44 +0100

    implemented server fetching again

--- nextjs_ssg/pages/_app.js
+++ nextjs_ssg/pages/_app.js
@@ -3,8 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-
-     /* if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  /* if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       btn_totop.style.display = "block";
     } else {
       btn_totop.style.display = "none";
@@ -12,18 +11,23 @@ function MyApp({ Component, pageProps }) {
   */
 
   // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
   return (
     <div className="wrapper">
       <Header />
       <Component {...pageProps} />
       <Footer />
-      <img id="btn_totop" src="/next (1).png" onClick={topFunction} className="btn_scroll" width="40px"/>
+      <img
+        id="btn_totop"
+        src="/next (1).png"
+        onClick={topFunction}
+        className="btn_scroll"
+        width="40px"
+      />
     </div>
   );
 }
